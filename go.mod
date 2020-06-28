19ba7fb4b5db2fb87757d372a761a2966e0a60c8
Assil Ksiksi <cyph0nik@gmail.com>
2020-06-28 15:38:50 -0400

    First commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/aksiksi/histweet
+
+go 1.14
+
+require github.com/dghubble/go-twitter v0.0.0-20190719072343-39e5462e111f
---
+++ histweet.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+
+    "github.com/dghubble/go-twitter/twitter"
+)
+
+func main() {
+    fmt.Println("Hello, world!")
+}
