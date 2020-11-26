794cd0fde44fe98c1cc59efb166cdbefb3e03ffa
Assil Ksiksi <cyph0nik@gmail.com>
2020-11-26 13:37:58 -0500

    Simple HTTP server for rule handling

---
+++ server/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/aksiksi/histweet/lib"
+)
+
+type ruleRequestJSON struct {
+	Rule     string `json:"rule"`
+	IsDaemon bool   `json:"is_daemon"`
+	Interval int    `json:"interval"`
+}
+
+type ruleResponseJSON struct {
+	Success bool   `json:"success"`
+	Msg     string `json:"msg"`
+}
+
+func ruleHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	parsed := ruleRequestJSON{}
+	decoder := json.NewDecoder(r.Body)
+
+	err := decoder.Decode(&parsed)
+	if err != nil {
+		resp, _ := json.Marshal(ruleResponseJSON{
+			Success: false,
+			Msg:     fmt.Sprintf("Invalid JSON request body: %s", err),
+		})
+
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(resp)
+
+		return
+	}
+
+	// Parse the Rule
+	parser := histweet.NewParser(parsed.Rule)
+	rule, err := parser.Parse()
+	if err != nil {
+		resp, _ := json.Marshal(ruleResponseJSON{
+			Success: false,
+			Msg:     fmt.Sprintf("Invalid rule string provided: %s", err),
+		})
+
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(resp)
+
+		return
+	}
+
+	fmt.Println(rule)
+
+	bytes := []byte(rule.ToString())
+	w.Write(bytes)
+}
+
+func main() {
+	http.HandleFunc("/rule", ruleHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
